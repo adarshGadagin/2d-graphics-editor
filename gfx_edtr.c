ffb5ae7531ab39e363322ca94eb43a1d25d08a82
adarshGadagin <adarshgadagin@gmail.com>
2026-06-06 23:30:58 +0530

    Add Menu, addShape and Display fuction

--- gfx_edtr.c
+++ gfx_edtr.c
@@ -4,52 +4,115 @@
 
 #define WIDTH 80
 #define HEIGHT 25 
+#define MAX_OBJ 50
+
+typedef enum {
+    LINE,
+    RECTANGLE,
+    CIRCLE,
+    TRIANGLE
+} ShapeType;
+
+// to store object information
+typedef struct{
+    ShapeType type;
+    int x1, y1, x2, y2;  // For line, rectangle, triangle
+    int x, y, radius;     // For circle
+    char symbol;
+    int active;           // 1 if active, 0 if deleted
+} Shape;
+
 
 typedef struct{
-    char canvas[HEIGHT][WIDTH];
-}size;
+    char picture[HEIGHT][WIDTH];
+    Shape objects[MAX_OBJ];
+    int objectCount;
+}picture;
 
+void initializePicture(picture *p);
 int isWithinBounds(int x,int y);
-void drawLine(size *c,int x1,int x2,int y1,int y2,char sym);
-void drawRectangle(size *c,int x1,int x2,int y1,int y2,char sym);
-void drawTriangle(size *c,int x1, int x2, int x, int y1, int y2, int y, char sym);
-void drawCircle(size *c,int r,int x,int y,char sym);
+void displayMenu();
+void addShape(picture *p);
+// void deleteShape(picture *p);
+// void redrawPicture(picture *p);
+// void savePicture(picture *p);
+// void loadPicture(picture *p);
+void displayPicture(picture *p);
+void drawLine(picture *p,int x1,int x2,int y1,int y2,char sym);
+void drawRectangle(picture *p,int x1,int x2,int y1,int y2,char sym);
+void drawTriangle(picture *p,int x1, int x2, int x, int y1, int y2, int y, char sym);
+void drawCircle(picture *p,int r,int x,int y,char sym);
+
 void drawObject();
 
-// typedef struct{
-//     int x1, x2, y1, y2;
-//     int x, y, r;
-// }input;
 
 int isWithinBounds(int x, int y){
     return (x>=0 && x<WIDTH && y>=0 && y<HEIGHT);
 }
 
-void drawObject(){
-    int x1, x2, y1, y2;
-    int x, y, r;
-    char sym;
-    int shape;
-    size c;
+int main(){
+    picture pic;
+    int choice;
+    int run=1;
 
-    printf("==============Shapes==============\n");
+    initializePicture(&pic);
+
+    while (run) {
+            displayMenu();
+            printf("\nEnter your choice: ");
+            scanf("%d", &choice);
+     
+            switch (choice) {
+                case 1:
+                    addShape(&pic);
+                    break;
+                case 2:
+                    // deleteShape(&pic);
+                    break;
+                case 3:
+                    displayPicture(&pic);
+                    break;
+                case 4:
+                    // savePicture(&pic);
+                    break;
+                case 5:
+                    // loadPicture(&pic);
+                    break;
+                case 6:
+                    initializePicture(&pic);
+                    break;
+                case 7:
+                    run = 0;
+                    break;
+                default:
+                    printf("\nInvalid choice! Please try again.\n");
+            }
+        }
+
+    return 0;
+}
+
+void addShape(picture *p) {
+    if (p->objectCount >= MAX_OBJ) {
+        printf("\nMaximum objects limit reached!\n");
+        return;
+    }
+ 
+    int shapeChoice, x1, y1, x2, y2, x3, y3, r, x, y;
+    char sym;
+ 
+    printf("\n========== ADD SHAPE ==========\n");
     printf("1. Line\n");
     printf("2. Rectangle\n");
-    printf("3. Triangle\n");
-    printf("4. Circle\n");
-    printf("Enter your choice: ");
-    scanf("%d",&shape);
+    printf("3. Circle\n");
+    printf("4. Triangle\n");
+    printf("Enter shape choice: ");
+    scanf("%d", &shapeChoice);
+ 
+    printf("Enter symbol to draw with (* or _): ");
+    scanf(" %c", &sym);
 
-    printf("Enter symbol to draw (* or _): ");
-    scanf(" %c",&sym);
-
-    for(int i=0;i<HEIGHT;i++){
-        for(int j=0;j<WIDTH;j++){
-            c.canvas[i][j]=' ';
-        }
-    }
-
-    switch (shape)
+    switch (shapeChoice)
     {
     case 1: // Line
         printf("Enter starting points (x1 y1): ");
@@ -57,7 +120,7 @@ void drawObject(){
         printf("Enter starting points (x2 y2): ");
         scanf("%d %d",&x2,&y2);
         if(isWithinBounds(x1,y1)&&isWithinBounds(x2,y2)){
-            drawLine(&c,x1,x2,y1,y2,sym);
+            drawLine(p,x1,x2,y1,y2,sym);
         }else{
             printf("Coordinates out of bound!(0-%d, 0-%d)",WIDTH-1,HEIGHT-1);
         }
@@ -69,7 +132,7 @@ void drawObject(){
         printf("Enter bottom right corner (x2 y2): ");
         scanf("%d %d",&x2,&y2);
         if(isWithinBounds(x1,y1)&&isWithinBounds(x2,y2)){
-            drawRectangle(&c,x1,x2,y1,y2,sym);
+            drawRectangle(p,x1,x2,y1,y2,sym);
         }else{
             printf("Coordinates out of bound!(0-%d, 0-%d)",WIDTH-1,HEIGHT-1);
         }
@@ -83,7 +146,7 @@ void drawObject(){
         printf("Enter third vertex (x3 y3): ");
         scanf("%d %d",&x,&y);
         if(isWithinBounds(x1,y1) && isWithinBounds(x2,y2) && isWithinBounds(x,y)){
-            drawTriangle(&c,x1,x2,x,y1,y2,y,sym);
+            drawTriangle(p,x1,x2,x,y1,y2,y,sym);
         }else{
             printf("one or more vertices out of bound!");
         }
@@ -95,26 +158,32 @@ void drawObject(){
         printf("Enter radius of circle: ");
         scanf("%d",&r);
         if(isWithinBounds(x,y)){
-            drawCircle(&c,r,x,y,sym);
+            drawCircle(p,r,x,y,sym);
         }else{
             printf("Center coordinates out of bound!");
         }
         break;
     
     default:
-    printf("Invalid choice! (1-4)");
+        printf("Invalid choice! (1-4)");
         break;
     }
 
-    for(int i=0;i<HEIGHT;i++){
-        for(int j=0;j<WIDTH;j++){
-            printf("%c",c.canvas[i][j]);
+}
+
+void initializePicture(picture *c) {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            c->picture[i][j] = ' ';
         }
-        printf("\n");
+    }
+    c->objectCount = 0;
+    for (int i = 0; i < MAX_OBJ; i++) {
+        c->objects[i].active = 0;
     }
 }
 
-void drawLine(size *c,int x1,int x2,int y1,int y2,char sym){
+void drawLine(picture *p,int x1,int x2,int y1,int y2,char sym){
     int dx=abs(x2-x1);
     int dy=abs(y2-y1);
     float slope=(float)(y2-y1)/(x2-x1);
@@ -122,7 +191,7 @@ void drawLine(size *c,int x1,int x2,int y1,int y2,char sym){
         int startY=(y1<y2)?y1:y2;
         int endY=(y1<y2)?y2:y1;
         for(int y=startY;y<endY;y++){
-           c->canvas[y][x1]='*';
+           p->picture[y][x1]='*';
         }
     }else{
         if(dx>=dy){
@@ -130,7 +199,7 @@ void drawLine(size *c,int x1,int x2,int y1,int y2,char sym){
             int endX=(x1<x2)?x2:x1;
             for(int x=startX;x<=endX;x++){
                 int y=(int)round(y1+slope*(x-x1));
-                c->canvas[y][x]=sym;
+                p->picture[y][x]=sym;
             }
         }else{
             int startY=(y1<y2)?y1:y2;
@@ -138,49 +207,78 @@ void drawLine(size *c,int x1,int x2,int y1,int y2,char sym){
             float inverse_slope=(float)(x2-x1)/(y2-y1);
             for(int y=startY;y<=endY;y++){
                 int x=(int)round(x1+inverse_slope*(y-y1));
-                c->canvas[y][x]=sym;
+                p->picture[y][x]=sym;
             }
         }
     }
 }
 
-void drawRectangle(size *c,int x1,int x2,int y1,int y2,char sym){
+void drawRectangle(picture *p,int x1,int x2,int y1,int y2,char sym){
     // top edge
     for(int x=x1;x<=x2;x++){
-        c->canvas[y1][x]=sym;
+        p->picture[y1][x]=sym;
     }
     // bottom edge
     for(int x=x1;x<=x2;x++){
-        c->canvas[y2][x]=sym;
+        p->picture[y2][x]=sym;
     }
     // left edge
     for(int y=y1;y<=y2;y++){
-        c->canvas[y][x1]=sym;
+        p->picture[y][x1]=sym;
     }
     // right edge
     for(int y=y1;y<=y2;y++){
-        c->canvas[y][x2]=sym;
+        p->picture[y][x2]=sym;
     }
 }
 
-void drawTriangle(size *c,int x1, int x2, int x, int y1, int y2, int y, char sym){
-    drawLine(c,x1,x2,y1,y2,sym);
-    drawLine(c,x2,x,y2,y,sym);
-    drawLine(c,x,x1,y,y1,sym);
+void drawTriangle(picture *p,int x1, int x2, int x, int y1, int y2, int y, char sym){
+    drawLine(p,x1,x2,y1,y2,sym);
+    drawLine(p,x2,x,y2,y,sym);
+    drawLine(p,x,x1,y,y1,sym);
 }
 
-void drawCircle(size *c, int r, int x, int y, char sym){
+void drawCircle(picture *p, int r, int x, int y, char sym){
     for(int j=y-r;j<=y+r;j++){
         for(int i=x-r;i<=x+r;i++){
             int dx=i-x;
             int dy=j-y;
             if(abs(dx*dx + dy*dy - r*r) <= r)
-                c->canvas[j][i]=sym;
+                p->picture[j][i]=sym;
         }
     }
 }
 
-int main(){
-    drawObject();
-    return 0;
+void displayMenu(){
+    printf("\n========== MENU ==========\n");
+    printf("1. Add Shape\n");
+    printf("2. Delete Shape\n");
+    printf("3. Display Picture\n");
+    printf("4. Save Picture\n");
+    printf("5. Load Picture\n");
+    printf("6. Clear Picture\n");
+    printf("7. Exit\n");
+    printf("=========================\n");
 }
+
+void displayPicture(picture *p) {
+    printf("\n");
+    printf("+");
+    for (int i = 0; i < WIDTH; i++) printf("-");
+    printf("+\n");
+ 
+    for (int i = 0; i < HEIGHT; i++) {
+        printf("|");
+        for (int j = 0; j < WIDTH; j++) {
+            printf("%c", p->picture[i][j]);
+        }
+        printf("|\n");
+    }
+ 
+    printf("+");
+    for (int i = 0; i < WIDTH; i++) printf("-");
+    printf("+\n");
+ 
+    printf("\nTotal objects: %d\n", p->objectCount);
+}
+
